e593cec6225c6547b8c6067e3d83652052f4088b
Fadi Touza <faditouza@icloud.com>
2024-07-26 20:02:30 +0200

    Added .gitignore file and imported lumibot dependencies into ML_trading_bot

---
+++ ML_trading_bot.py
@@ -0,0 +1,14 @@
+from lumibot.brokers import Alpaca                      #Broker
+from lumibot.backtesting import YahooDataBacktesting    #Backtesting
+from lumibot.strategies.strategy import Strategy        #The trading bot itself
+from lumibot.traders import Trader                      #Gives deployemnt capability
+from datetime import datetime
+
+print(3)
+
+API_KEY = ""
+API_SECRET = ""
+BASE_URL = ""
+
+
+
