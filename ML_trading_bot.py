b82402707a64cc68b43742c027c3be7863e150bf
Fadi Touza <faditouza@icloud.com>
2024-08-04 19:30:31 +0200

    Added dynamic position sizing with 'cash at risk' and set 'take profit' and 'stop loss' on trade. \n Still only put a single trade

--- ML_trading_bot.py
+++ ML_trading_bot.py
@@ -18,23 +18,36 @@ ALPACA_CREDS = {
 class TradingStrategy(Strategy):
     """Contains the core strategy for the training bo t"""
 
-    def initialize(self, symbol:str = "SPY"):
-        """Runs once when bot starts trading"""
+    def initialize(self, symbol:str="SPY", cash_at_risk:float=0.5):
         self.symbol = symbol
         self.sleeptime = "24H"
+        self.cash_at_risk = cash_at_risk
         self.last_trade = None
     
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = round((cash * self.cash_at_risk) / last_price)
+        return cash, last_price, quantity
+
     def on_trading_iteration(self):
+        cash, last_price, quantity = self.position_sizing()
+
+        ### Check if fractional trading is supported
         if self.last_trade == None:
-            order = self.create_order(self.symbol, 7, "buy", type="market")
+            order = self.create_order(self.symbol, quantity, "buy", type="bracket",
+                                      take_profit_price = last_price * 1.15, stop_loss_price = last_price * 0.90)
             self.submit_order(order)
             self.last_trade = "buy"
 
+
+
+
 start_date = datetime(2022,1,1)
-end_date = datetime(2023,12,30)
+end_date = datetime(2022,3,28)
 
 broker = Alpaca(ALPACA_CREDS)
 strategy = TradingStrategy(name="RocketTrader", broker=broker,
-                           parameters={"symbol":"SPY"})
+                           parameters={"symbol":"SPY", "cash_at_risk":0.5})
 strategy.backtest(YahooDataBacktesting, start_date, end_date,
-                  parameters={"symbol":"SPY"})
+                  parameters={"symbol":"SPY", "cash_at_risk":0.5})
